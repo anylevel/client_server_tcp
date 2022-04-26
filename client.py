4ef0adcfdf6bb97dd6031dd98c5e6952e78981a2
anylevel <vadyusha.surin@gmail.com>
2022-04-26 09:49:11 +0300

    worked!

--- client.py
+++ client.py
@@ -2,15 +2,28 @@ import socket
 
 
 def main():
-    target_host = int(input("enter the server address"))
-    target_port = int(input("Enter the server port"))
+    target_host = input("Enter the server address:")
+    target_port = int(input("Enter the server port:"))
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((target_host, target_port))
+    try:
+        client.connect((target_host, target_port))
+    except OSError:
+        print("Server not found")
+        exit(1)
     print("Enter 'quit' to log out from the server")
-    message = input()
+    message = input('Enter message:')
     while message != 'quit':
-        client.send(message)
-        response = client.recv(4096)
-        print(response.decode())
-        message = input()
+        try:
+            client.send(message.encode())
+            response = client.recv(4096)
+            print(response.decode())
+            message = input('Enter message:')
+        except KeyboardInterrupt:
+            print('SIGINT:Client close connection')
+            break
     client.close()
+    return 0
+
+
+if __name__ == '__main__':
+    main()
